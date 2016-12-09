b9d1a2af786d92ad1c0e433d6842b1c8199ebdc3
Shayan Shams <Shayan Shams>
2016-12-09 23:33:10 +0100

    correction index.html to index.php

--- Microservices_de/navigation.php
+++ Microservices_de/navigation.php
@@ -1,7 +1,7 @@
 <?php
 	echo'<nav class="nav">
 			<ul>
-				<li><a href="index.html">Home</a></li>
+				<li><a href="index.php">Home</a></li>
 				<li><a href="Microservices_de/log_In.php">An-Abmeldung</a></li>
 				<li><a href="#">Einstellung</a></li>
 				<li><a href="#">Photos/Videos</a></li>
