6ade4b0b97f39d27504c71cffa38ab42caf6f6e2
Shayan Shams <Shayan Shams>
2016-12-09 23:59:31 +0100

    add meta.php to Microservices_de

---
+++ Microservices_de/meta.php
@@ -0,0 +1,11 @@
+<?php
+	echo '<meta charset=UTF-8>
+		<link rel="stylesheet" href="Design/style.css">
+		<meta charset="UTF-8">
+		<!-- Webseite deffinieren: -->
+		<meta name="description" content="Projekt1(Weblog)">
+		<!-- Keywords für die Suchmaschine definieren: -->
+		<meta name="keywords" content="HTML, CSS, PHP, Weblog">
+		<!-- Autor deffinieren: -->
+		<meta name="author" content="Shayan">';
+?>
