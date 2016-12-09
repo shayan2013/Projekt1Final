b6946d3f9e28021ab1e22d93a7078af8c2efbd71
Shayan Shams <Shayan Shams>
2016-12-09 21:02:37 +0100

    add navigation.php to Microservices_de

---
+++ Microservices_de/navigation.php
@@ -0,0 +1,10 @@
+<?php
+	echo'<nav class="nav">
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="Microservices_de/log_In.php">An-Abmeldung</a></li>
+				<li><a href="#">Einstellung</a></li>
+				<li><a href="#">Photos/Videos</a></li>
+			</ul>
+		</nav>';
+?>
