9eac816667fcc65a589d0410e0dc37a81840ffd1
Shayan Shams <Shayan Shams>
2016-12-09 20:51:09 +0100

    add logIn.php-link to index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ echo GREETING;
 			<nav class="nav">
 				<ul>
 					<li><a href="index.html">Home</a></li>
-					<li><a href="#">An-Abmeldung</a></li>
+					<li><a href="Microservices_de/log_In.php">An-Abmeldung</a></li>
 					<li><a href="#">Einstellung</a></li>
 					<li><a href="#">Photos/Videos</a></li>
 				</ul>
