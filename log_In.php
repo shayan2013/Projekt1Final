d3e7c48d4dfa054c4efb92678b546decf301178c
Shayan Shams <Shayan Shams>
2016-12-23 16:29:33 +0100

    use sessionUnset in log_In.php

--- log_In.php
+++ log_In.php
@@ -17,7 +17,7 @@
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		if (isset($_POST['logOut'])) {
-			sessionDestroy();
+			sessionUnset("uname");
 		} else {
 			if (empty($_POST["name"])) {
 				$nameErr = "Name erforderlich!";
