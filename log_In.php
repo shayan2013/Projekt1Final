de2de1f57073adc238f94614c1287d27b1041de3
Shayan Shams <Shayan Shams>
2016-12-23 19:56:57 +0100

    use selectOne-function to write data into Datensicherung.txt

--- log_In.php
+++ log_In.php
@@ -1,66 +1,102 @@
-<?php include 'Microservices_de/session.php';?>
 <?php include 'Microservices_de/headerFooter.php';?>
 <?php include 'Microservices_de/dbConfig.php';?>
+<?php include 'Microservices_de/fileCreater.php';?>
 <!DOCTYPE HTML>  
 <html lang=de>
 	<head>
 		<?php include 'Microservices_de/meta.php';?>
-		<title>Log In/Out</title>
+		<title>Register</title>
 	</head>
 	<body>  
 
 	<?php
 	// Variablen mit leeren Values erstellen
-	$nameErr = $kennwortErr = "";
-	$name = $kennwort = "";
+	$nameErr = $emailErr = $geschlechtErr = $kennwortErr = $kennwort2Err = $websiteErr = "";
+	$name = $email = $geschlecht = $kennwort = $kennwort2 = $kommentar = $website = "";
 	$x = 0;
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		if (isset($_POST['logOut'])) {
-			sessionUnset("uname");
-		} else {
-			if (empty($_POST["name"])) {
-				$nameErr = "Name erforderlich!";
-				 // testen ob der Name nur aus Buchstaben und Leertaste besteht
-			 } elseif (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-				  $nameErr = "Nur Buchstaben und Leertaste erlaubt!"; 
-			 }else {
-				$name = test_input($_POST["name"]);
-				$x++;
-			 }
-			  
-			 if (empty($_POST["kennwort"])) {
-				$kennwortErr = "kennwort erforderlich!";
-			 } else {
-				$kennwort = $_POST["kennwort"];
-				$x++;
-		     }
-			  
-		     if ($x == 2) {
-				create();
-				$y = logInCheck($name, $kennwort);
-			    if ($y == 1){
-					sessionSetName($name);
-					header("Location: index.php");
-				}
-				$x = 0;
-			  }
-			  		  
-			}
-
+	  if (empty($_POST["name"])) {
+		$nameErr = "Name erforderlich!";
+		 // testen ob der Name nur aus Buchstaben und Leertaste besteht
+	  } elseif (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+		  $nameErr = "Nur Buchstaben und Leertaste erlaubt!"; 
+		}else {
+		$name = test_input($_POST["name"]);
+		$x++;
+	  }
+	  
+	  if (empty($_POST["email"])) {
+		$emailErr = "Email erforderlich!";
+			// testen ob Emailadresse gültige Form entspricht
+	  } elseif (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		  $emailErr = "ungültige Email-format"; 
+		}else {
+		$email = test_input($_POST["email"]);
+		$x++;
 		}
 		
-			function test_input($data) {
-				  $data = trim($data);
-				  $data = stripslashes($data);
-				  $data = htmlspecialchars($data);
-				  return $data;
-			 }	
+		
+
+	  if (empty($_POST["kennwort"])) {
+		$kennwortErr = "kennwort erforderlich!";
+	  } else {
+		$kennwort = $_POST["kennwort"];
+		$x++;
+	  }
+	  
+	  if (empty($_POST["kennwort2"])) {
+		$kennwort2Err = "kennwort wiederholen bitte!";
+	  } elseif ($kennwort != $_POST["kennwort2"]) {
+		$kennwort2Err = "Kennwort stimmt nicht überein!";
+	  }else {
+		  $kennwort2 = $_POST["kennwort2"];
+		  $x++;
+	  }
+	  
+	  if (empty($_POST["website"])) {
+		$website = "";
+	  } else {
+		$website = test_input($_POST["website"]);
+		if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+		  $websiteErr = "ungültige URL"; 
+		  $x--;
+		}
+	  }
+
+	  if (empty($_POST["kommentar"])) {
+		$kommentar = "";
+	  } else {
+		$kommentar = test_input($_POST["kommentar"]);
+	  }
+
+	  if (empty($_POST["geschlecht"])) {
+		$geschlechtErr = "Geschlecht erforderlich!";
+	  } else {
+		$geschlecht = test_input($_POST["geschlecht"]);
+		$x++;
+	  }
+	}
+
+	function test_input($data) {
+	  $data = trim($data);
+	  $data = stripslashes($data);
+	  $data = htmlspecialchars($data);
+	  return $data;
+	}
 	
+	if ($x == 5) {
+		create();
+		$lastId = createData($name, $email, $kennwort, $website, $kommentar, $geschlecht);
+		$text = selectOne($lastId);
+		writeData($text);
+		$x = 0;
+		header("Location: log_In.php");
+	}
 	?>
 		<div class="flex-container">
 			<header>
-				<h1>Log In</h1>
+				<h1>Anmeldeformular</h1>
 				<?php headi();?>
 			</header>
 			<?php include 'Microservices_de/navigation.php';?>
@@ -69,17 +105,26 @@
 			  Username: <input type="text" name="name" value="<?php echo $name;?>">
 			  <span class="error">* <?php echo $nameErr;?></span>
 			  <br><br>
+			  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+			  <span class="error">* <?php echo $emailErr;?></span>
+			  <br><br>
 			  Kennwort: <input type="password" name="kennwort" value="<?php echo $kennwort;?>">
 			  <span class="error">* <?php echo $kennwortErr;?></span>
 			  <br><br>
-			  <?php 
-				if (isset($_SESSION["uname"])) { 
-				 echo '<input type="submit" name="logOut" value="Log Out">';  
-			   } else { 
-				 echo '<input type="submit" name="LogIn" value="Log In">';  
-			   } 
-			   ?>
-			  <p>noch nicht registiert? <a href="register.php"> klick hier!</a></P>
+			  Kennwort(Wiederholung): <input type="password" name="kennwort2" value="<?php echo $kennwort2;?>">
+			  <span class="error">* <?php echo $kennwort2Err;?></span>
+			  <br><br>
+			   Website: <input type="text" name="website" value="<?php echo $website;?>">
+			  <span class="error"><?php echo $websiteErr;?></span>
+			  <br><br>
+			   Kommentar: <textarea name="kommentar" rows="5" cols="40"><?php echo $kommentar;?></textarea>
+			  <br><br>
+			  geschlecht:
+			  <input type="radio" name="geschlecht" <?php if (isset($geschlecht) && $geschlecht=="weiblich") echo "checked";?> value="weiblich">weiblich
+			  <input type="radio" name="geschlecht" <?php if (isset($geschlecht) && $geschlecht=="männlich") echo "checked";?> value="männlich">männlich
+			  <span class="error">* <?php echo $geschlechtErr;?></span>
+			  <br><br>
+			  <input type="submit" name="submit" value="Absenden">  
 			</form>
 			<footer><?php footi();?></footer>
 		</div>
@@ -88,8 +133,15 @@
 	echo "<h2>Deine Eingabe:</h2>";
 	echo $name;
 	echo "<br>";
+	echo $email;
+	echo "<br>";
 	echo $kennwort;
 	echo "<br>";
+	echo $website;
+	echo "<br>";
+	echo $kommentar;
+	echo "<br>";
+	echo $geschlecht;
 	echo $x;
 	?>
 
