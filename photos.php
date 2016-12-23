32c067b131a7965360fe388fdeed180db890e839
Shayan Shams <Shayan Shams>
2016-12-23 20:16:20 +0100

    create photos.php

---
+++ photos.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>  
+<html lang=de>
+	<head>
+		<?php include 'Microservices_de/meta.php';?>
+		<title>Log In/Out</title>
+	</head>
+	<body> 
+		<?php include 'Microservices_de/imageConf.php';?>
+	</body>
+</html>
