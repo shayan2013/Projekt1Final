474e29872650c8fd0ddd0d97e3fb595d836660b1
Shayan Shams <Shayan Shams>
2016-12-28 13:50:59 +0100

    create einstellung.php

---
+++ einstellung.php
@@ -0,0 +1,23 @@
+<?php include 'Microservices_de/session.php';?>
+<?php include 'Microservices_de/headerFooter.php';?>
+<?php include 'Microservices_de/dbConfig.php';?>
+<!DOCTYPE HTML>  
+<html lang=de>
+	<head>
+		<?php include 'Microservices_de/meta.php';?>
+		<title>Einstellung</title>
+	</head>
+	<body>  
+		<div class="flex-container">
+			<header>
+				<h1>Einstellung</h1>
+				<?php headi();?>
+			</header>
+			<?php include 'Microservices_de/navigation.php';?>
+			
+			
+			<footer><?php footi();?></footer>
+		</div>
+
+	</body>
+</html>
