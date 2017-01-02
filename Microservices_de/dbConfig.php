4d761bfdd15a7cc7190cac26ca9479d46f1e7885
Shayan Shams <Shayan Shams>
2017-01-02 14:06:17 +0100

    fetch_assoc() in updateData

--- Microservices_de/dbConfig.php
+++ Microservices_de/dbConfig.php
@@ -113,26 +113,32 @@ function deleteData($id){
 function updateData($uname, $eMail, $kenn, $id) {
 	global $servername, $username, $password, $dbname;
 	$conn = new mysqli($servername, $username, $password, $dbname);
-	if (empty($uname){
-		$sql = "SELECT username FROM users WHERE id ='$id'"
-		$uname = $conn->query($sql);
+	$sql = "SELECT username, email, kennwort FROM users WHERE id = " . $id ;
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		while ($row = $result->fetch_assoc()) {
+			if (empty($uname)){
+				$uname = $row["username"];
+			}
+			if (empty($eMail)){
+				$eMail = $row["email"];
+			}
+			if (empty($kenn)){
+				$kenn = $row["kennwort"];
+			}
+		}
+	} else {
+		return false;
 	}
-	if (empty($eMail){
-		$sql = "SELECT email FROM users WHERE id ='$id'"
-		$eMail = $conn->query($sql);
-	}
-	if (empty($kenn){
-		$sql = "SELECT kennwort FROM users WHERE id ='$id'"
-		$kenn = $conn->query($sql);
-	}
+	
 	$sql = "UPDATE users SET username = ?, email = ?, kennwort = ? WHERE id = ?";
 	$stmt = $conn->prepare($sql);
 	$stmt->bind_param('sssi', $uname, $eMail, $kenn, $id);
 	$stmt->execute();
 	if ($conn->query($sql) === TRUE) {
-		echo "Daten erfolgreich aktualisiert.";
+		return true;
 	} else {
-		echo "Daten nicht aktualisiert: " . $conn->error;
+		return false;
 	}
 	$stmt->close();
 	$conn->close();
